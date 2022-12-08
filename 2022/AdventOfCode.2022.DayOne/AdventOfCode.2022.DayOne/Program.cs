fb96242ecc30223b5a1541d3cff878d3c52e03d7
Leonid Idelchik <leonid.idelchik@gmail.com>
2022-12-08 22:15:31 +0300

    part 1

---
+++ 2022/AdventOfCode.2022.DayOne/AdventOfCode.2022.DayOne/Program.cs
@@ -0,0 +1,17 @@
+﻿var maxTotalValue = int.MinValue;
+var currentTotal = 0;
+
+foreach (var line in File.ReadLines("dump.txt"))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (currentTotal > maxTotalValue)
+            maxTotalValue = currentTotal;
+
+        currentTotal = 0;
+        continue;
+    }
+
+    var value = int.Parse(line);
+    currentTotal += value;
+}
