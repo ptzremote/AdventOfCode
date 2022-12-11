4427aa4492092838bf911961c93b0675ff70a30d
Leonid Idelchik <leonid.idelchik@gmail.com>
2022-12-11 19:57:52 +0300

    day 4 - part 1

---
+++ 2022/AdventOfCode.2022.Day4/Program.cs
@@ -0,0 +1,19 @@
+﻿var result = 0;
+
+foreach(var line in File.ReadLines("dump.txt")) {
+    var parts = line.Split(',');
+
+    var firstSections = parts[0].Split('-');
+    var firstSectionStart = int.Parse(firstSections[0]);
+    var firstSectionEnd = int.Parse(firstSections[1]);
+
+    var secondSections = parts[1].Split('-');
+    var secondSectionStart = int.Parse(secondSections[0]);
+    var secondSectionEnd = int.Parse(secondSections[1]);
+
+    if ((firstSectionStart <= secondSectionStart && firstSectionEnd  >= secondSectionEnd) ||
+        (firstSectionStart >= secondSectionStart && firstSectionEnd <= secondSectionEnd))
+        result++;
+}
+
+Console.WriteLine(result);
