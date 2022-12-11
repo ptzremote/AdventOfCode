90ed337c9862ccd0d9d4cbe636393bb1b85bea7b
Leonid Idelchik <leonid.idelchik@gmail.com>
2022-12-11 18:39:19 +0300

    part 1

---
+++ 2022/AdventOfCode.2022.DayThree/AdventOfCode.2022.DayThree/Program.cs
@@ -0,0 +1,21 @@
+﻿var result = 0;
+
+var index = 0;
+var alphabetDict = Enumerable.Range('a', 26).Concat(Enumerable.Range('A', 26))
+    .Select(num => ((char)num)).ToDictionary(@char => @char, @char => ++index);
+
+foreach (var rucksack in File.ReadLines("dump.txt"))
+{
+    var compartmentOne = rucksack[..(rucksack.Length / 2)];
+    var compartmentTwo = rucksack[(rucksack.Length / 2)..];
+
+    var hash = compartmentTwo.ToHashSet();
+
+    foreach (var @char in compartmentOne.Where(@char => hash.Contains(@char)))
+    {
+        result += alphabetDict[@char];
+        break;
+    }
+}
+
+Console.WriteLine(result);
