acc3354a13e8487b80d43ecd88a223a9147dee97
Leonid Idelchik <leonid.idelchik@gmail.com>
2022-12-12 21:20:24 +0300

    day 6 - part 1

---
+++ 2022/AdventOfCode.2022.Day6/Program.cs
@@ -0,0 +1,20 @@
+﻿var chars = File.ReadAllText("dump.txt").ToCharArray();
+var markerCount = 4;
+
+var startIndex = 0;
+var queue = new Queue<char>();
+
+foreach (var @char in chars)
+{
+	startIndex++;
+
+	queue.Enqueue(@char);
+
+	if (queue.Count > markerCount)
+		queue.Dequeue();
+
+	if (queue.ToHashSet().Count == markerCount)
+		break;
+}
+
+Console.WriteLine(startIndex);
