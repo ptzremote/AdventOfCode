867998e306fdc3e141fb2bd00f8bf234c55627de
Leonid Idelchik <leonid.idelchik@gmail.com>
2022-12-17 15:53:05 +0300

    day 8 - part 1

---
+++ 2022/AdventOfCode.2022.Day8/Program.cs
@@ -0,0 +1,81 @@
+﻿var firstLineProcessed = false;
+var forest = new int[0, 0];
+
+var visibleTrees = new HashSet<TreePosition>();
+
+var lineIndex = 0;
+foreach (var line in File.ReadLines("dump.txt"))
+{
+    var trees = line.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
+
+    if (firstLineProcessed == false)
+    {
+        forest = new int[trees.Length, trees.Length];
+    }
+
+    for (int i = 0; i < trees.Length; i++)
+    {
+        forest[lineIndex, i] = trees[i];
+    }
+
+    lineIndex++;
+    firstLineProcessed = true;
+}
+
+for(int line = 0; line < forest.GetLength(0); line++)
+{
+    var minHeight = int.MinValue;
+    for (int column = 0; column < forest.GetLength(0); column++)
+    {
+        var currentTreeHeight = forest[line, column];
+
+        if (currentTreeHeight > minHeight)
+        {
+            minHeight = currentTreeHeight;
+            visibleTrees.Add(new TreePosition(line, column));
+        }
+    }
+
+    minHeight = int.MinValue;
+    for (int column = forest.GetLength(0) - 1; column >= 0; column--)
+    {
+        var currentTreeHeight = forest[line, column];;
+
+        if (currentTreeHeight > minHeight)
+        {
+            minHeight = currentTreeHeight;
+            visibleTrees.Add(new TreePosition(line, column));
+        }
+    }
+}
+
+for (int column = 0; column < forest.GetLength(0); column++)
+{
+    var minHeight = int.MinValue;
+    for (int line = 0; line < forest.GetLength(0); line++)
+    {
+        var currentTreeHeight = forest[line, column];
+
+        if (currentTreeHeight > minHeight)
+        {
+            minHeight = currentTreeHeight;
+            visibleTrees.Add(new TreePosition(line, column));
+        }
+    }
+
+    minHeight = int.MinValue;
+    for (int line = forest.GetLength(0) - 1; line >= 0; line--)
+    {
+        var currentTreeHeight = forest[line, column];
+
+        if (currentTreeHeight > minHeight)
+        {
+            minHeight = currentTreeHeight;
+            visibleTrees.Add(new TreePosition(line, column));
+        }
+    }
+}
+
+Console.WriteLine(visibleTrees.Count);
+
+public record TreePosition(int X, int Y);
